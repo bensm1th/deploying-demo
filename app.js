8f77a70c6cd8f47ea25b6320213308f4e47f69ad
Benjamin Smith <ben.smith@10klt.com>
2016-09-16 13:59:12 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app     = express(),
+    port    = process.env.PORT || '3000';
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.listen(port, function() {
+    console.log('app listening on port 3000');
+});
